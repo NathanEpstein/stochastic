316a98c9f230293788ddfbfde304b7dac28305c4
Jason Walsh <j@wal.sh>
2016-09-13 11:37:26 -0400

    Type tests

---
+++ src/types.js
@@ -0,0 +1,26 @@
+Number.isInteger = Number.isInteger || function(value) {
+    return typeof value === 'number' &&
+        isFinite(value) &&
+        Math.floor(value) === value;
+};
+
+Math.sign = Math.sign || function(x) {
+    x = +x; // convert to a number
+    if (x === 0 || isNaN(x)) {
+        return x;
+    }
+    return x > 0 ? 1 : -1;
+};
+
+let isNonNegative = {n} {
+    let s = Math.sign(n);
+    return s === 1 || s === 0;
+}
+
+let isMatrix = (m) => {
+    return Array.isArray(m) && m.every((e) => Array.isArray(e));
+}
+
+let isSymmetricMatrix = (m) => {
+    return isMatrix(m) && m.every((e) => e.length === m.length);
+}
