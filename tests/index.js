b6a929dde9edd2084b7b2c9035a72313e3340083
Jason Walsh <j@wal.sh>
2016-12-01 10:45:56 -0600

    Initial spec

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,8 @@
 import { spec, valid, explain, conform } from 'js.spec';
+
 import * as stoch from '../src/index';
 
 console.log(stoch);
 console.log(spec);
+
+console.log('norm is an Array', spec.array(stoch.norm(1, 1, 100)));
