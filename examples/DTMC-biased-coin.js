70150a1b49cdcbaaf1257aa412d1fd9030d120db
Jason Walsh <j@wal.sh>
2016-12-02 10:21:54 -0600

    Simplify example of biased coin flips

--- examples/DTMC-biased-coin.js
+++ examples/DTMC-biased-coin.js
@@ -1,44 +1,33 @@
 // Problem: Determine when a biased coin has been switched
 import * as stoch from '../src/index';
 
-
 // Is the biased or fair coin used
 const coinMode = [
         [0.9, 0.1],
         [0.1, 0.9]
 ];
 
-// Heads and Tails equally likely
-const fairCoin = [
-        [0.5, 0.5],
-        [0.5, 0.5]
-];
-
-// Heads 80% of the time
-const biasedCoin = [
-        [0.8, 0.2],
-        [0.2, 0.8]
-];
-
-
 // Visualize the ground true for when a coin has been switch between fair and biased
 // Use 1000 as the base example
 const activeCoin = stoch.DTMC(coinMode, 1000, 0, true);
-const stateCoin = ['H', 'T'];
-const fairCoinFlips = stoch.DTMC(fairCoin, 1000, 0, true);
-const biasedCoinFlips = stoch.DTMC(biasedCoin, 1000, 0, true);
-
 // console.log(activeCoin);
+const stateCoin = ['H', 'T'];
 
 const flips = activeCoin.map((e, i, c) => {
-  let result;
-  if (e === 0) {
-    result = fairCoinFlips.pop();
-  } else {
-    result = biasedCoinFlips.pop();
+  // Fair
+  let result = Math.random() < .5 ? 0 : 1;
+  // e is 0: fair or 1: biased
+  if (e === 1) {
+    // Biased
+    result = Math.random() < .8 ? 0 : 1;
   }
   return stateCoin[result];
 });
 
-console.log(flips);
+const dist = flips.reduce((p, c) => {
+  p[c] =  p[c] ?  ++p[c] : 1;
+  return p;
+}, {H: 0, T: 0});
+console.log(dist);
+
 // Test for when Baum-Welch 1000 Viterbi 1000
